0380b19d63dc366b897b56c52f4769f643f89ca7
Rustu (nucofu-laptop) <rusjud1ka@gmail.com>
2026-08-30 13:19:16 +0700

    komit komit komit
    Signed-off-by: Rustu (nucofu-laptop) <rusjud1ka@gmail.com>

--- .config/selfScript/statusBar.sh
+++ .config/selfScript/statusBar.sh
@@ -3,90 +3,94 @@
 # required dependencies
 # echo, awk, date, cat, sensors, brightnessctl, amixer, wmctrl, ls, iw, sleep, lemonbar
 
-primary_bright_color="#19bcff"
-primary_normal_color="#19abff"
-primary_low_color="#199aff"
-
-background_color="#991e1e2e"
+primary="#19bcff"
+background_color="#99000000"
 border_color="#282828"
 
 red_color="#de1009"
 green_color="#10de09"
 yellow_color="#ffe74f"
 
-waktu() {
-    kalender="$(date +"%A, %d %B")"
-    jam="$(date +"%R")"
+dateNclock() {
+    current_date="$(date +"%A, %d %B")"
+    current_clock="$(date +"%R")"
 
-    out_a="%{F$primary_bright_color}$jam%{F-} $kalender"
+    # output variable
+    out_a="%{F$primary}$current_clock%{F-} $current_date"
 }
 
-baterai() {
-    baterai="$(cat /sys/class/power_supply/BAT1/capacity)"
+battery() {
+    bat="$(cat /sys/class/power_supply/BAT1/capacity)"
     status="$(cat /sys/class/power_supply/BAT1/status)"
-    title=""
+    bat_line=""
 
+    # Battery Threshold Color Indicator
     if [[ "$status" == "Charging" ]]; then
-        title="%{F$green_color}Bat%{F-}"
+        bat_line="%{F$green_color}Bat%{F-}"
     elif [[ "$status" == "Full" ]]; then
-        title="%{F$green_color}Bat%{F-}"
+        bat_line="%{F$green_color}Bat%{F-}"
     else
-       if [ $baterai -lt 20 ]; then
-           title="%{F$red_color}Bat%{F-}"
+       if [ $bat -lt 20 ]; then
+           bat_line="%{F$red_color}Bat%{F-}"
        else
-          title="%{F$primary_bright_color}Bat%{F-}"
+          bat_line="%{F$primary}Bat%{F-}"
        fi
     fi
 
-    out_b="$title $baterai%"
+    # output variable
+    out_b="$bat_line $bat%"
 
 }
 
 ram() {
     ram="$(awk '/MemTotal/{t=$2}/MemAvailable/{a=$2}END{print int((t-a)*100/t)}' /proc/meminfo)"
 
+    # Ram Threshold Color Indicator & output variable
     if [ $ram -gt 80 ]; then
         out_c="%{F$red_color}RAM%{F-} $ram%"
     else
-        out_c="%{F$primary_bright_color}RAM%{F-} $ram%"
+        out_c="%{F$primary}RAM%{F-} $ram%"
     fi
 }
 
 temp() {
-    temp="$(sensors coretemp-isa-0000 | awk -F"[+|°]" '/Core 0:/ { print $2 }')"
-    temp_int=${temp%.*}
+    temp="$(sensors coretemp-isa-0000 | awk '/Core/ {gsub(/[+°C]/, "", $3); sum += $3; count++} END {if (count > 0) print int((sum/count) + 0.5) "°C"; else print "N/A"}')"
 
-    if [ $temp_int -gt 55 ]; then
-        out_d="%{F$red_color}Temp%{F-} $temp_int°C"
+    # Temp Threshold Color Indicator & output variable
+    if [ $temp -gt 55 ]; then
+        out_d="%{F$red_color}Temp%{F-} $temp"
     else
-        out_d="%{F$primary_bright_color}Temp%{F-} $temp_int°C"
+        out_d="%{F$primary}Temp%{F-} $temp"
     fi
     
 }
 
 backlight() {
     backlight="$(brightnessctl | awk -F"[(|%]" '/Current brightness: / {print $2}')"
-    
-    out_e="%{A4:brightnessctl -q set +5%:}%{A5:brightnessctl -q set 5%-:}%{F$primary_bright_color}Bright%{F-} $backlight%%%{A4}%{A5}"
+
+    # output variable
+    out_e="%{A4:brightnessctl -q set +5%:}%{A5:brightnessctl -q set 5%-:}%{F$primary}Bright%{F-} $backlight%%%{A4}%{A5}"
 }
 
 volume() {
-    amout="$(amixer -M get Master)"
-    volume="$(echo "$amout" | awk -F"[][%]" '/Left:/ { print $2 }')"
-    status="$(echo "$amout" | awk -F"[][]" '/Left:/ { print $4 }')"
+    volume="$(amixer -M get Master | awk -F"[][]" '/Playback/ && /%/ {sum += $2; count++} END {if (count > 0) print int(sum/count) "%"; else print "0%"}')"
+    status="$(amixer -M get Master | awk -F"[][]" '/Playback/ && /%/ {if ($6 == "off" || $4 == "off") muted=1} END {if (muted) print "off"; else print "on"}')"
 
-    [ "$status" == "off" ] && out_f="%{F$primary_bright_color}Vol%{F-} %{F$red_color}MUTED%{F-}" && return
-    
-    out_f="%{A4:amixer -q sset Master 5%+:}%{A5:amixer -q sset Master 5%-:}%{F$primary_bright_color}Vol%{F-} $volume%%%{A4}%{A5}"
+    # show MUTED if status is off
+    [ "$status" == "off" ] && out_f="%{F$primary}Vol%{F-} %{F$red_color}MUTED%{F-}" && return
+
+    # output variable
+    out_f="%{A4:amixer -q sset Master 5%+:}%{A5:amixer -q sset Master 5%-:}%{F$primary}Vol%{F-} $volume%%{A4}%{A5}"
 }
 
 workspace() {
     unset out_g
-    list_w="$(wmctrl -d)"
+
+    # coloring active desktop & output variable
     for i in 0 1 2 3; do
-        dat="$(echo "$list_w" | awk -v idx="$i" '$1 == idx {print $2}')"
-        if [ "$dat" == "*" ]; then
-            out_g+=" %{F$primary_bright_color}$i%{F-} "
+        check_desktop="$(wmctrl -d | awk -v idx="$i" '$1 == idx {print $2}')"
+        if [ "$check_desktop" == "*" ]; then
+            out_g+=" %{F$primary}$i%{F-} "
         else
             out_g+=" %{A:wmctrl -s $i:}$i%{A} "
         fi
@@ -94,7 +98,7 @@ workspace() {
 }
 
 cpu() {
-    total="$(awk '/cpu / {print int($1+$2+$3+$4+$5+$6+$7+$8+$9+$10+$11)}' /proc/stat)"
+    total="$(awk '/cpu / {print int($2+$3+$4+$5+$6+$7+$8+$9+$10+$11)}' /proc/stat)"
     idle="$(awk '/cpu / {print int($5+$6)}' /proc/stat)"
 
     diff_total=$((total - prev_total))
@@ -102,55 +106,51 @@ cpu() {
 
     cpu_usage=$((((diff_total - diff_idle) * 100) / diff_total))
 
+    # CPU Threshold Color Indicator & output variable
     if [ $cpu_usage -gt 80 ]; then
         out_h="%{F$red_color}CPU%{F-} $cpu_usage%"
     else
-        out_h="%{F$primary_bright_color}CPU%{F-} $cpu_usage%"
+        out_h="%{F$primary}CPU%{F-} $cpu_usage%"
     fi
-    
+
+    # update previous variable
     prev_total=$total
     prev_idle=$idle
 }
 
 wifi() {
-    # ambil interface wireless pertama
-    iface="$(ls /sys/class/net 2>/dev/null | grep '^wl' | head -n1)"
-    [ -z "$iface" ] && out_i="%{A:cmst > /dev/null 2>&1 &:}%{F$primary_bright_color}$iface%{F-} down%{A}" && return
+    # check wireless interface
+    [ -z "$iface" ] && out_i="%{A:cmst > /dev/null 2>&1 &:}%{F$primary}$iface%{F-} down%{A}" && return
 
     # wifi state
     state="$(cat /sys/class/net/$iface/operstate 2>/dev/null)"
 
     # connected SSID
-    connSSID="$(iw dev $iface link | awk '/SSID/{name=$2}END{if (name != "") {print " ["name"] "} else {print " "}}')"
-    
+    connSSID="$(iw dev $iface link | awk '/SSID: / {sub(/.*SSID: /, ""); print}')"
+
+    # condition for output variable
     if [ "$state" != "up" ]; then
-        out_i="%{A:cmst > /dev/null 2>&1 &:}%{F$primary_bright_color}$iface%{F-} down%{A}"
+        # if device is down
+        out_i="%{A:cmst > /dev/null 2>&1 &:}%{F$primary}$iface%{F-} down%{A}"
         return
     else
         if [ "$connSSID" != " " ]; then                
-            # strength (%)
-            strength="$(iw dev $iface link | awk '/signal/{dbm=$2}END{if (dbm != "") {print int(((dbm+100)*100)/60)}}')"
+            # strength with dbm
+            strength="$(iw dev $iface link | awk '/signal/{dbm=$2}END{if (dbm != "") {print int(dbm)}}')"
             [ -z "$strength" ] && strength=0
-            
-            #limit strength
-            if [ $strength -gt 100 ]; then
-                strength=+100
-            elif [ $strength -lt 0 ]; then
-                strength=-0
-            fi
-            
+                        
             # Strength Color Indicator
-            if [ $strength -gt 70 ]; then
-                ipi="%{A:cmst > /dev/null 2>&1 &:}%{F$green_color}$iface$connSSID%{F-}${strength}%%%{A}"
-            elif [ $strength -lt 30 ]; then
-                ipi="%{A:cmst > /dev/null 2>&1 &:}%{F$red_color}$iface$connSSID%{F-}${strength}%%%{A}"
+            if [ $strength -gt -40 ]; then
+                out_i="%{A:cmst > /dev/null 2>&1 &:}%{F$primary}$iface [%{F-}%{F$green_color$connSSID%{F-}%{F$primary}]%{F-} ${strength} dbm%{A}"
+            elif [ $strength -lt -80 ]; then
+                out_i="%{A:cmst > /dev/null 2>&1 &:}%{F$primary}$iface [%{F-}%{F$red_color}$connSSID%{F-}%{F$primary}]%{F-} ${strength} dbm%{A}"
             else
-                ipi="%{A:cmst > /dev/null 2>&1 &:}%{F$primary_bright_color}$iface$connSSID%{F-}${strength}%%%{A}"
+                out_i="%{A:cmst > /dev/null 2>&1 &:}%{F$primary}$iface [%{F-}$connSSID%{F-}%{F$primary}]%{F-} ${strength} dbm%{A}"
             fi
-            out_i="$ipi"
 
         else
-            out_i="%{A:cmst > /dev/null 2>&1 &:}%{F$primary_bright_color}$iface%{F-} up%{A}"
+            # if device is up
+            out_i="%{A:cmst > /dev/null 2>&1 &:}%{F$primary}$iface%{F-} up%{A}"
             return
         fi
     fi
@@ -171,10 +171,7 @@ traffic_bytes() {
     prev_rx=$rx
     prev_tx=$tx
 
-    rx=0
-    tx=0
-
-    # auto convert K/M
+    # auto convert Kb to Mb
     if [ "$rx_diff" -gt 1024 ]; then
         rx_out="$((rx_diff/1024))Mb"
     else
@@ -187,31 +184,40 @@ traffic_bytes() {
         tx_out="${tx_diff}Kb"
     fi
 
-    out_j="%{F$primary_bright_color}d/u%{F-} ${rx_out}/${tx_out}"
+    # output variable
+    out_j="%{F$primary}d/u%{F-} ${rx_out}/${tx_out}"
 
 }
 
 # CPU previous stat
-prev_total="$(awk '/cpu / {print int($1+$2+$3+$4+$5+$6+$7+$8+$9+$10+$11)}' /proc/stat)"
+prev_total="$(awk '/cpu / {print int($2+$3+$4+$5+$6+$7+$8+$9+$10+$11)}' /proc/stat)"
 prev_idle="$(awk '/cpu / {print int($5+$6)}' /proc/stat)"
 
-# interface, upload and download previous stat
-iface="$(ls /sys/class/net 2>/dev/null | grep '^wl' | head -n1)"
+# wireless interface
+iface="$1"
+if [ ! -e "/sys/class/net/$iface" ]; then
+    echo "Error: interface '$iface' tidak ditemukan" >&2
+    exit 1
+fi
+
+# upload and download previous stat
 prev_rx="$(cat /sys/class/net/$iface/statistics/rx_bytes 2>/dev/null)"
 prev_tx="$(cat /sys/class/net/$iface/statistics/tx_bytes 2>/dev/null)"
 
+# separator
 separator="%{F$yellow_color}|%{F-}"
 
+# main loop
 while true; do
     workspace
     wifi
     ram
     cpu
-    baterai
+    battery
     temp
     volume
     backlight
-    waktu
+    dateNclock
     traffic_bytes
     
     echo "$out_g %{r}$out_i $separator $out_j $separator $out_c $separator $out_h $separator $out_b $separator $out_d $separator $out_f $separator $out_e $separator $out_a "
--- README.md
+++ README.md
@@ -2,26 +2,29 @@
 my linux dotfiles configuration
 
 current
-- conky
+- conky **updated**
 - fastfetch
-- nitrogen
-- openbox
+- nitrogen **updated**
+- openbox **updated**
 - picom
 - sakura
 - dunst
 - obmenu-generator
-- lemonbar
+- lemonbar **updated**
+- qt5ct, qt6ct
 
 extra
 - themes
 - icons
 
 next
-- qt5ct, qt6ct
-- conky **update**
 - dunst **update**
+- i3lock
+
+aborted
 - battery management
-- screen mirror
+- screen mirror (different repo)
 
 catetan
-*kahade adjust path ka desktop dina obmenu-generator lamun ek ganti user
+- *kahade adjust path ka desktop dina obmenu-generator lamun ek ganti user
+- *update, di script statusbar wajib nambahkeun argumen iface wireless devices laptop ente
